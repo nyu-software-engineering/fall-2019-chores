6e2c0ac50c15eca359ed49e0790d0faae5c9637d
Mert Alev <ma4759@nyu.edu>
2019-10-29 19:26:16 -0400

    Minor changes to database schema

--- src/db.js
+++ src/db.js
@@ -1,87 +1,84 @@
-const mongoose = require('mongoose');
-const URLSlugs = require('mongoose-url-slugs');
+const mongoose = require("mongoose");
+const URLSlugs = require("mongoose-url-slugs");
+
+const Chore = new mongoose.Schema({
+  criteria: {
+    type: [String],
+    required: false,
+    min: 1
+  },
+  late: {
+    type: Boolean,
+    required: false
+  },
+  //0: unassigned
+  //1: assigned, incomplete
+  //2: completed
+  status: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 2
+  },
+  title: {
+    type: String,
+    required: true,
+    minlength: 3,
+    trim: true
+  }
+});
 
 const Household = new mongoose.Schema({
-	chores: [Chores],
-	members: [Person],
-	title: {
-		type: String,
-		required: true,
-		minLength: 3,
-		trim: true
-	}
+  chores: [Chore],
+  members: [Person],
+  title: {
+    type: String,
+    required: true,
+    minLength: 3,
+    trim: true
+  }
 });
 
 const Person = new mongoose.Schema({
-	assigned: {
-		type: [Chore],
-		required: true
-	},
-	household: {
-		type: Household,
-		required: true
-	},
-	name: {
-		type: String,
-		required: true,
-		minLength: 1,
-		trim: true
-	},
-	phoneNum: {
-		type: String,
-		required: true,
-		minLength: 10
-	},
-	score: {
-        type: Number,
-        required: true,
-		min: 1,
-		max: 5
-	}
+  assigned: {
+    type: [Chore],
+    required: false,
+    min: 1
+  },
+  household: {
+    type: [Household],
+    required: false
+  },
+  name: {
+    type: String,
+    required: true,
+    minLength: 1,
+    trim: true
+  },
+  phoneNum: {
+    type: String,
+    required: true,
+    minlength: 10,
+    maxlength: 10
+  },
+  score: {
+    type: Number,
+    required: false,
+    min: 1,
+    max: 5
+  }
 });
 
-const Chore = new mongoose.Schema({
-	assignedTo: {
-		type: Person,
-		required: false,
-	},
-	criteria: {
-        type: [String],
-        required: false,
-        min: 1
-	},
-	late: {
-		type: Boolean,
-		required: true
-	},
-	//0: unassigned
-	//1: assigned, incomplete
-	//2: completed
-	status: {
-		type: Number,
-		required: true,
-		min: 0,
-		max: 2,
-	},
-    title: {
-        type: String,
-        required: true,
-		minlength: 3,
-		trim: true
-    }
-});
-
-Household.plugin(URLSlugs('title'));
+Household.plugin(URLSlugs("title"));
+Person.plugin(URLSlugs("name"));
 const houseModel = {
-	Chore: mongoose.model('Chore', Chore),
-	Person: mongoose.model('Person', Person),
-    Household: mongoose.model('Household', Household)
+  Chore: mongoose.model("Chore", Chore),
+  Person: mongoose.model("Person", Person),
+  Household: mongoose.model("Household", Household)
 };
 
-mongoose.connect(
-    "mongodb://localhost/housekeeper", {
-        useNewUrlParser: true
-    }
-);
+mongoose.connect("mongodb://localhost/housekeeper", {
+  useNewUrlParser: true
+});
 
-module.exports = houseModel; 
+module.exports = houseModel;
