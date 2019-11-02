8ba336776369cfc409c6f75a1e718220e31aa387
Mert Alev <ma4759@nyu.edu>
2019-11-01 20:18:34 -0400

    Major changes to schema and db.js

--- src/db.js
+++ src/db.js
@@ -1,85 +1,13 @@
 const mongoose = require("mongoose");
-const URLSlugs = require("mongoose-url-slugs");
+// const Chore = require("../src/chore").Chore;
+// const Household = require("../src/household").Household;
+const Person = require("../src/person").Person;
 
-const ChoreSchema = new mongoose.Schema({
-  criteria: {
-    type: [String],
-    required: false,
-    min: 1
-  },
-  late: {
-    type: Boolean,
-    required: false
-  },
-  //0: unassigned
-  //1: assigned, incomplete
-  //2: completed
-  status: {
-    type: Number,
-    required: true,
-    min: 0,
-    max: 2
-  },
-  title: {
-    type: String,
-    required: true,
-    minlength: 3,
-    trim: true
-  }
-});
-
-const HouseholdSchema = new mongoose.Schema({
-  chores: [ChoreSchema],
-  title: {
-    type: String,
-    required: true,
-    minlength: 3,
-    maxlength: 20,
-    trim: true
-  }
-});
-
-const PersonSchema = new mongoose.Schema({
-  assigned: {
-    type: [ChoreSchema],
-    required: false,
-    min: 1
-  },
-  household: {
-    type: [HouseholdSchema],
-    required: false
-  },
-  name: {
-    type: String,
-    required: true,
-    minLength: 1,
-    trim: true
-  },
-  phoneNum: {
-    type: String,
-    required: true,
-    minlength: 10,
-    maxlength: 10
-  },
-  score: {
-    type: Number,
-    required: false,
-    min: 1,
-    max: 5
-  }
-});
-
-HouseholdSchema.plugin(URLSlugs("title"));
-PersonSchema.plugin(URLSlugs("name"));
-const houseModel = {
-  ChoreSchema: mongoose.model("ChoreSchema", ChoreSchema),
-  PersonSchema: mongoose.model("PersonSchema", PersonSchema),
-  HouseholdSchema: mongoose.model("HouseholdSchema", HouseholdSchema)
-};
+mongoose.model("Chore", Chore);
+// mongoose.model("Household", Household);
+// mongoose.model("Person", Person);
 
 mongoose.connect("mongodb://localhost/housekeeper", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-
-module.exports = houseModel;
