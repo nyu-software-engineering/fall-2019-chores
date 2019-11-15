107c642533a21a6eb975a8984d793565f40fa8b5
Rena Auerbach <renaauerbach@gmail.com>
2019-11-15 10:01:02 -0500

    Updated Account form page

--- src/views/UserAccount.jsx
+++ src/views/UserAccount.jsx
@@ -32,154 +32,189 @@ import Button from '../components/CustomButton.jsx';
 
 export default class UserAccount extends Component {
    render() {
+      const states = [
+         'AL',
+         'AK',
+         'AZ',
+         'AR',
+         'CA',
+         'CO',
+         'CT',
+         'DE',
+         'DC',
+         'FL',
+         'GA',
+         'HI',
+         'ID',
+         'IL',
+         'IN',
+         'IA',
+         'KS',
+         'KY',
+         'LA',
+         'ME',
+         'MD',
+         'MA',
+         'MI',
+         'MN',
+         'MS',
+         'MO',
+         'MT',
+         'NE',
+         'NV',
+         'NH',
+         'NJ',
+         'NM',
+         'NY',
+         'NC',
+         'ND',
+         'OH',
+         'OK',
+         'OR',
+         'PA',
+         'RI',
+         'SC',
+         'SD',
+         'TN',
+         'TX',
+         'UT',
+         'VT',
+         'VA',
+         'WA',
+         'WV',
+         'WI',
+         'WY',
+      ];
       return (
          <div className="content">
             <Container fluid>
                <Row>
-                  <Col md={8}>
-                     <Card
-                        title="Edit Profile"
-                        content={
-                           <form>
-                              <FormInputs
-                                 ncols={['col-md-5', 'col-md-3', 'col-md-4']}
-                                 properties={[
-                                    {
-                                       label: 'Company (disabled)',
-                                       type: 'text',
-                                       bsClass: 'form-control',
-                                       placeholder: 'Company',
-                                       defaultValue: 'Creative Code Inc.',
-                                       disabled: true,
-                                    },
-                                    {
-                                       label: 'Username',
-                                       type: 'text',
-                                       bsClass: 'form-control',
-                                       placeholder: 'Username',
-                                       defaultValue: 'michael23',
-                                    },
-                                    {
-                                       label: 'Email address',
-                                       type: 'email',
-                                       bsClass: 'form-control',
-                                       placeholder: 'Email',
-                                    },
-                                 ]}
-                              />
-                              <FormInputs
-                                 ncols={['col-md-6', 'col-md-6']}
-                                 properties={[
-                                    {
-                                       label: 'First name',
-                                       type: 'text',
-                                       bsClass: 'form-control',
-                                       placeholder: 'First name',
-                                       defaultValue: 'Mike',
-                                    },
-                                    {
-                                       label: 'Last name',
-                                       type: 'text',
-                                       bsClass: 'form-control',
-                                       placeholder: 'Last name',
-                                       defaultValue: 'Andrew',
-                                    },
-                                 ]}
-                              />
-                              <FormInputs
-                                 ncols={['col-md-12']}
-                                 properties={[
-                                    {
-                                       label: 'Adress',
-                                       type: 'text',
-                                       bsClass: 'form-control',
-                                       placeholder: 'Home Adress',
-                                       defaultValue:
-                                          'Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09',
-                                    },
-                                 ]}
-                              />
-                              <FormInputs
-                                 ncols={['col-md-4', 'col-md-4', 'col-md-4']}
-                                 properties={[
-                                    {
-                                       label: 'City',
-                                       type: 'text',
-                                       bsClass: 'form-control',
-                                       placeholder: 'City',
-                                       defaultValue: 'Mike',
-                                    },
-                                    {
-                                       label: 'Country',
-                                       type: 'text',
-                                       bsClass: 'form-control',
-                                       placeholder: 'Country',
-                                       defaultValue: 'Andrew',
-                                    },
-                                    {
-                                       label: 'Postal Code',
-                                       type: 'number',
-                                       bsClass: 'form-control',
-                                       placeholder: 'ZIP Code',
-                                    },
-                                 ]}
-                              />
+                  <Card
+                     title="My Account"
+                     lineBreak
+                     content={
+                        <form>
+                           <FormInputs
+                              ncols={['col-md-4', 'col-md-8']}
+                              properties={[
+                                 {
+                                    label: 'Username',
+                                    type: 'text',
+                                    bsClass: 'form-control',
+                                    placeholder: 'Username',
+                                    defaultValue: 'Mert123',
+                                    required: true,
+                                 },
+                                 {
+                                    label: 'Email Address',
+                                    type: 'email',
+                                    bsClass: 'form-control',
+                                    placeholder: 'Email',
+                                    required: true,
+                                 },
+                              ]}
+                           />
+                           <FormInputs
+                              ncols={['col-md-6', 'col-md-6']}
+                              properties={[
+                                 {
+                                    label: 'First Name',
+                                    type: 'text',
+                                    bsClass: 'form-control',
+                                    placeholder: 'First Name',
+                                    defaultValue: 'Mert',
+                                    required: true,
+                                 },
+                                 {
+                                    label: 'Last Name',
+                                    type: 'text',
+                                    bsClass: 'form-control',
+                                    placeholder: 'Last Name',
+                                    defaultValue: 'Alev',
+                                    required: true,
+                                 },
+                              ]}
+                           />
+                           <FormInputs
+                              ncols={['col-md-12']}
+                              properties={[
+                                 {
+                                    label: 'Street Address',
+                                    type: 'text',
+                                    bsClass: 'form-control',
+                                    placeholder: 'Street Adress',
+                                    defaultValue: '123 E. 12th Street, Apt 4D',
+                                    required: true,
+                                 },
+                              ]}
+                           />
+                           <FormInputs
+                              ncols={[
+                                 'col-md-4',
+                                 'col-md-2',
+                                 'col-md-3',
+                                 'col-md-3',
+                              ]}
+                              properties={[
+                                 {
+                                    label: 'City',
+                                    type: 'text',
+                                    bsClass: 'form-control',
+                                    placeholder: 'City',
+                                    defaultValue: 'New York',
+                                    required: true,
+                                 },
+                                 {
+                                    label: 'State',
+                                    type: 'text',
+                                    as: 'select',
+                                    options: { states },
+                                    bsClass: 'form-control',
+                                    placeholder: 'State',
+                                    defaultValue: 'NY',
+                                 },
+                                 {
+                                    label: 'Country',
+                                    type: 'text',
+                                    bsClass: 'form-control',
+                                    placeholder: 'Country',
+                                    defaultValue: 'USA',
+                                    required: true,
+                                 },
+                                 {
+                                    label: 'Postal Code',
+                                    type: 'number',
+                                    bsClass: 'form-control',
+                                    placeholder: 'Zip Code',
+                                    required: true,
+                                 },
+                              ]}
+                           />
 
-                              <Row>
-                                 <Col md={12}>
-                                    <FormGroup controlId="formControlsTextarea">
-                                       <FormLabel>About Me</FormLabel>
-                                       <FormControl
-                                          rows="5"
-                                          componentClass="textarea"
-                                          bsClass="form-control"
-                                          placeholder="Here can be your description"
-                                          defaultValue="Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo."
-                                       />
-                                    </FormGroup>
-                                 </Col>
-                              </Row>
-                              <Button pullRight fill type="submit">
-                                 Update Profile
-                              </Button>
-                              <div className="clearfix" />
-                           </form>
-                        }
-                     />
-                  </Col>
-                  <Col md={4}>
-                     <UserCard
-                        bgImage="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400"
-                        name="Mike Andrew"
-                        userName="michael24"
-                        description={
-                           <span>
-                              "Lamborghini Mercy
-                              <br />
-                              Your chick she so thirsty
-                              <br />
-                              I'm in that two seat Lambo"
-                           </span>
-                        }
-                        socials={
-                           <div>
-                              <Button simple>
-                                 <i className="fa fa-facebook-square" />
-                              </Button>
-                              <Button simple>
-                                 <i className="fa fa-twitter" />
-                              </Button>
-                              <Button simple>
-                                 <i className="fa fa-google-plus-square" />
-                              </Button>
-                           </div>
-                        }
-                     />
-                  </Col>
+                           <Row>
+                              <Col md={12}>
+                                 <FormGroup controlId="formControlsTextarea">
+                                    <FormLabel>About Me</FormLabel>
+                                    <FormControl
+                                       rows="5"
+                                       componentClass="textarea"
+                                       bsClass="form-control"
+                                       placeholder="Enter any info you want housemates to know about you"
+                                       defaultValue="Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo."
+                                    />
+                                 </FormGroup>
+                              </Col>
+                           </Row>
+                           <Button pullRight fill type="submit">
+                              Save
+                           </Button>
+                           <div className="clearfix" />
+                        </form>
+                     }
+                  />
                </Row>
             </Container>
          </div>
       );
    }
 }
-
