a47ed8fca9228126eb71a69e18fe498ff6aae7b8
Mert Alev <ma4759@nyu.edu>
2019-12-02 02:34:40 -0500

    Experimental database integration for signup form

--- src/views/Signup.jsx
+++ src/views/Signup.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import {
-   Col,
-   Container,
-   FormControl,
-   FormGroup,
-   FormLabel,
-   Row,
+	Col,
+	Container,
+	FormControl,
+	FormGroup,
+	FormLabel,
+	Row,
 } from 'react-bootstrap';
 import { withRouter } from 'react-router-dom';
 
@@ -19,128 +19,169 @@ import HomePage from '../views/HomePage';
 import UserCard from '../components/UserCard';
 
 class Signup extends Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-         date: new Date(),
-         email: '',
-         password: '',
-         firstName: '',
-         lastName: '',
-         // person: new person(),
-      };
-      this.routeChange = this.routeChange.bind(this);
-   }
+	constructor(props) {
+		super(props);
+		this.state = {
+			firstName: '',
+			lastName: '',
+			phoneNum: '',
+			password: '',
+			title: ''
+		};
 
-   routeChange() {
-      let path = `/home`;
-      this.props.history.push(path);
-   }
+		this.routeChange = this.routeChange.bind(this);
+		this.sendData = this.sendData.bind(this);
+	}
 
-   validateForm() {
-      return this.state.email.length > 0 && this.state.password.length > 0;
-   }
+	async sendData() {
+		await fetch('/api/household/', {
+			method: 'post',
+			body: JSON.stringify({
+				title: this.title
+			}),
+			headers: {
+				'Content-Type': 'application/json',
+			}
+		})
+		.then(res => res.json())
+		.then(status => {
+			if(status.success === false) {
+				//show failure page
+			} else {
+				await fetch('/api/person', {
+					method: 'post',
+					body: JSON.stringify([{
+						phoneNum: this.phoneNum,
+						firstName: this.firstName,
+						lastName: this.lastName,
+					}, {
+						id: status.id
+					}]),
+					headers: {
+						'Content-Type': 'application/json',
+					}
+				})
+				.then(res => res.json())
+				.then(status => {
+					if(status.success === false) {
+						//show failure page
+					} else {
+						//show success page
+					}
+				});
+			}
+		});
 
-   onChange = date => this.setState({ date });
+		
+	}
 
-   render() {
-      return (
-         <div id="signup" className="signup">
-            <div className="header">
-               <h4>HouseKeeper</h4>
-            </div>
-            <div className="content">
-               <Container fluid>
-                  <Row>
-                     <Col md={{ span: 5, offset: 3 }}>
-                        <Card
-                           title="Sign Up"
-                           lineBreak
-                           content={
-                              <form>
-                                 <FormInputs
-                                    cols={['col-md-6', 'col-md-6']}
-                                    properties={[
-                                       {
-                                          as: 'input',
-                                          bsPrefix: 'form-control',
-                                          label: 'First Name',
-                                          placeholder: 'First Name',
-                                          required: true,
-                                          size: 'sm',
-                                          type: 'text',
-                                          // value: { state.firstName },
-                                          // onChange: {(e) => this.handleChange(e)}
-                                       },
-                                       {
-                                          as: 'input',
-                                          bsPrefix: 'form-control',
-                                          label: 'Last Name',
-                                          placeholder: 'Last Name',
-                                          required: true,
-                                          size: 'sm',
-                                          type: 'text',
-                                       },
-                                    ]}
-                                 />
-                                 <FormInputs
-                                    cols={['col-md-12']}
-                                    properties={[
-                                       {
-                                          as: 'input',
-                                          bsPrefix: 'form-control',
-                                          label: 'Email Address',
-                                          placeholder: 'Email',
-                                          required: true,
-                                          size: 'sm',
-                                          type: 'email',
-                                       },
-                                    ]}
-                                 />
-                                 <FormInputs
-                                    cols={['col-md-6', 'col-md-6']}
-                                    properties={[
-                                       {
-                                          as: 'input',
-                                          bsPrefix: 'form-control',
-                                          label: 'Password',
-                                          placeholder: 'Password',
-                                          required: true,
-                                          size: 'sm',
-                                          type: 'password',
-                                       },
-                                       {
-                                          as: 'input',
-                                          bsPrefix: 'form-control',
-                                          label: 'Confirm Password',
-                                          placeholder: 'Confirm Password',
-                                          required: true,
-                                          size: 'sm',
-                                          type: 'password',
-                                       },
-                                    ]}
-                                 />
-                                 <Button
-                                    block
-                                    onClick={this.routeChange}
-                                    size="md"
-                                    type="submit"
-                                    // disabled={!this.validateForm()}
-                                    variant="success"
-                                 >
-                                    Sign Up
-                                 </Button>
-                                 <div className="clearfix" />
-                              </form>
-                           }
-                        />
-                     </Col>
-                  </Row>
-               </Container>
-            </div>
-         </div>
-      );
-   }
+	routeChange() {
+		let path = `/home`;
+		this.props.history.push(path);
+	}
+
+	validateForm() {
+		return this.state.email.length > 0 && this.state.password.length > 0;
+	}
+
+	render() {
+		return (
+			<div id="signup" className="signup">
+				<div className="header">
+					<h4>HouseKeeper</h4>
+				</div>
+				<div className="content">
+					<Container fluid>
+						<Row>
+							<Col md={{ span: 5, offset: 3 }}>
+								<Card
+									title="Sign Up"
+									lineBreak
+									content={
+										<form>
+											<FormInputs
+												cols={['col-md-6', 'col-md-6']}
+												properties={[
+													{
+														as: 'input',
+														bsPrefix: 'form-control',
+														label: 'First Name',
+														placeholder: 'First Name',
+														required: true,
+														size: 'sm',
+														type: 'text',
+														// value: { state.firstName },
+														// onChange: {(e) => this.handleChange(e)}
+													},
+													{
+														as: 'input',
+														bsPrefix: 'form-control',
+														label: 'Last Name',
+														placeholder: 'Last Name',
+														required: true,
+														size: 'sm',
+														type: 'text',
+													},
+												]}
+											/>
+											<FormInputs
+												cols={['col-md-12']}
+												properties={[
+													{
+														as: 'input',
+														bsPrefix: 'form-control',
+														label: 'Email Address',
+														placeholder: 'Email',
+														required: true,
+														size: 'sm',
+														type: 'email',
+													},
+												]}
+											/>
+											<FormInputs
+												cols={['col-md-6', 'col-md-6']}
+												properties={[
+													{
+														as: 'input',
+														bsPrefix: 'form-control',
+														label: 'Password',
+														placeholder: 'Password',
+														required: true,
+														size: 'sm',
+														type: 'password',
+													},
+													{
+														as: 'input',
+														bsPrefix: 'form-control',
+														label: 'Confirm Password',
+														placeholder: 'Confirm Password',
+														required: true,
+														size: 'sm',
+														type: 'password',
+													},
+												]}
+											/>
+											<Button
+												block
+												onClick={this.routeChange}
+												size="md"
+												type="submit"
+												// disabled={!this.validateForm()}
+												variant="success"
+											>
+												Sign Up
+											</Button>
+											<div className="clearfix" />
+										</form>
+									}
+								/>
+							</Col>
+						</Row>
+					</Container>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default withRouter(Signup);
