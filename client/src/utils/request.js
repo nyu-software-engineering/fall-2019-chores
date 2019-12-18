5bb4caa7e3bb16c120082b4cb6accfdf41368cc1
eugene-choi <eugenechoi618@gmail.com>
2019-12-17 23:52:51 -0500

    Finished exports for axios.

--- client/src/utils/request.js
+++ client/src/utils/request.js
@@ -31,3 +31,26 @@ const handleResponse = ({ data }) => ({ response: data });
 const handleError = async ({ response: { data } }) => ({
   error: data,
 });
+
+export default {
+  get: (url, params = {}) =>
+    axios
+      .get(url, { params })
+      .then(handleResponse)
+      .catch(handleError),
+  post: (url, payload) =>
+    axios
+      .post(url, payload)
+      .then(handleResponse)
+      .catch(handleError),
+  put: (url, payload) =>
+    axios
+      .put(url, payload)
+      .then(handleResponse)
+      .catch(handleError),
+  delete: url =>
+    axios
+      .delete(url)
+      .then(handleResponse)
+      .catch(handleError),
+};
